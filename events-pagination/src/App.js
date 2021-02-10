516dd9bc4973972d250de1cfa42e1c98dbd252d9
Taven Shumaker <tavensshumaker@gmail.com>
2021-02-10 12:57:15 -0800

    set up events-pagination app structure

---
+++ events-pagination/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import YearContainer from './Components/YearContainer.js';
+
+function App() {
+  return (
+    <div className="App">
+      <YearContainer year='2019'/>
+      <YearContainer year='2020'/>
+    </div>
+  );
+}
+
+export default App;
