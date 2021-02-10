8ecedff31b3c952a8ad666fe2c547582326aa377
Taven Shumaker <tavensshumaker@gmail.com>
2021-02-10 13:04:31 -0800

    set up sample data in the testing utils file

---
+++ events-pagination/src/__test__/EP-testingUtils.js
@@ -0,0 +1,35 @@
+export const sampleData = [
+  {
+  classification: "event",
+  id: "ocd-event/cc66cc1d-a825-4ad9-8e27-a73f4be8c9af",
+  end_date: "",
+  all_day: false,
+  description: "",
+  status: "passed",
+  updated_at: "2020-04-05T20:03:39.915919+00:00",
+  start_date: "2019-12-05T18:00:00+00:00",
+  name: "Crenshaw Project Corporation"
+},
+{
+  classification: "event",
+  id: "ocd-event/a50e4741-c602-4344-af13-a862b90dba39",
+  end_date: "",
+  all_day: false,
+  description: "",
+  status: "passed",
+  updated_at: "2020-04-05T20:09:13.481338+00:00",
+  start_date: "2019-12-05T18:00:00+00:00",
+  name: "Regular Board Meeting"
+},
+{
+  classification: "event",
+  id: "ocd-event/dc9ecbf8-f925-4f5e-b5e6-ded4c17b49f0",
+  end_date: "",
+  all_day: false,
+  description: "",
+  status: "cancelled",
+  updated_at: "2020-04-05T20:05:22.744171+00:00",
+  start_date: "2019-12-04T18:30:00+00:00",
+  name: "Measure M Independent Taxpayer Oversight Committee"
+}
+]
